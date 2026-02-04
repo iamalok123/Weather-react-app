52a4c26e93a7ad1da9459da7d321fad806d8f95a
Alok <alokhotta10@gmail.com>
2026-02-04 23:09:21 +0530

    fix

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,8 +1,8 @@
-import windImg from "../assets/wind.png";
+import windImg from "../assets/Wind.png";
 import humidityImg from "../assets/humidity.png";
 import visibilityImg from "../assets/visibility.png";
-import sunriseImg from "../assets/sunrise.png";
-import sunsetImg from "../assets/sunset.png";
+import sunriseImg from "../assets/Sunrise.png";
+import sunsetImg from "../assets/Sunset.png";
 
 const Icon = ({ src, alt }) => <img src={src} alt={alt} className="h-8 w-8" />;
 
